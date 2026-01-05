d9154c9305e35be50b102c9922305f7ae616c31f
Maksym Prykhodko <m.prykhodko424@gmail.com>
2026-01-05 23:37:12 +0100

    domens

--- upsl-back/database_service/app/main.py
+++ upsl-back/database_service/app/main.py
@@ -13,9 +13,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3001",  # Next.js
-        "http://localhost:3000",  # Next.js
-
+        "0.0.0.0",
     ],
     allow_credentials=True,
     allow_methods=["*"],
